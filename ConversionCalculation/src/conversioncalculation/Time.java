703ee19d7d0531cc0c55cb4b1843c297f96d5f5c
escanor155 <46752934+escanor155@users.noreply.github.com>
2023-11-14 07:37:09 +0800

    Update Time.java

--- ConversionCalculation/src/conversioncalculation/Time.java
+++ ConversionCalculation/src/conversioncalculation/Time.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package conversioncalculation;
 
-/**
- *
- * @author zagar
- */
 public class Time {
     private double millisecond;
     private double second;
