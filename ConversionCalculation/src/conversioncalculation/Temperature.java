b05b6cefa6807af5e3b63abaf18cb19d3311c69d
escanor155 <46752934+escanor155@users.noreply.github.com>
2023-11-14 07:36:27 +0800

    Update Temperature.java

--- ConversionCalculation/src/conversioncalculation/Temperature.java
+++ ConversionCalculation/src/conversioncalculation/Temperature.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package conversioncalculation;
 
-/**
- *
- * @author zagar
- */
 public class Temperature {
     private double fahrenheit;
     private double celsius;
